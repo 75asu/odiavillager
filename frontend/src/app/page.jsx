690939bb1f7fd272601ecdeda8590703f24bfc65
Asutosh Panda <78539272+75asu@users.noreply.github.com>
2025-07-04 08:01:14 +0000

    adjust home and README

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,12 +1,12 @@
 "use client"
-import SubscriptionBox from "@/components/subscription/SubscriptionBox";
-import SubscriptionPlan from "@/components/subscription/SubscriptionPlan";
+// import SubscriptionBox from "@/components/subscription/SubscriptionBox";
+// import SubscriptionPlan from "@/components/subscription/SubscriptionPlan";
 import HomeBanner from "@/components/home/HomeBanner";
-import HomeExperience from "@/components/home/HomeExperience";
-import HomeMovieCategory from "@/components/home/HomeMovieCategory";
-import AskedQuestion from "@/components/question/AskedQuestions";
+// import HomeExperience from "@/components/home/HomeExperience";
+// import HomeMovieCategory from "@/components/home/HomeMovieCategory";
+// import AskedQuestion from "@/components/question/AskedQuestions";
 import HomeTitle from "@/components/home/HomeTitle";
-import { useState } from "react";
+// import { useState } from "react";
 
 export default function Home() {
   return (
@@ -15,15 +15,15 @@ export default function Home() {
 
       <HomeTitle />
 
-      <HomeMovieCategory />
+      {/* <HomeMovieCategory /> */}
 
-      <HomeExperience />
+      {/* <HomeExperience /> */}
 
-      <AskedQuestion />
+      {/* <AskedQuestion /> */}
 
-      <SubscriptionPlan />
+      {/* <SubscriptionPlan /> */}
 
-      <SubscriptionBox />
+      {/* <SubscriptionBox /> */}
 
     </>
   );
--- frontend/src/components/home/HomeTitle.jsx
+++ frontend/src/components/home/HomeTitle.jsx
@@ -5,15 +5,14 @@ import Link from "next/link";
 const HomeTitle = () => {
     return (
         <section className="relative -top-20 pt-4">
-            <h2 className="font-semibold text-center 2xl:text-5xl lg:text-4.5xl md:text-4xl text-2xl mb-4 text-white">The Best Streaming Experience</h2>
+            <h2 className="font-semibold text-center 2xl:text-5xl lg:text-4.5xl md:text-4xl text-2xl mb-4 text-white">Eii website bisayare kahaku kahibani</h2>
 
             <p
                 className="text-c-grey-60 text-center 3xl:text-super-base lg:text-sm md:text-sm text-xs mb-5 
           mx-auto xl:w-3/5 md:w-3/4 max-md:px-10 max-sm:px-5"
             >
-                StreamVibe is the best streaming experience for watching your favorite movies and shows on demand, anytime, anywhere. With
-                StreamVibe, you can enjoy a wide variety of content, including the latest blockbusters, classic movies, popular TV shows, and more. <br />
-                You can also create your own watchlists, so you can easily find the content you want to watch.
+                Coolest Odia Content At One Place <br />
+
             </p>
             <Link href="/explore">
                 <button
