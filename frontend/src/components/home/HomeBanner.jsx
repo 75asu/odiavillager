6e1d7fcf112a5b72feac12dccf508e6ca628ee18
Asutosh Panda <78539272+75asu@users.noreply.github.com>
2025-07-05 04:57:25 +0000

    change banner

--- frontend/src/components/home/HomeBanner.jsx
+++ frontend/src/components/home/HomeBanner.jsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const HomeBanner = () => {
     return (
-        <div className="w-full h-screen relative">
-            <Image src="/images/Group 1.jpg" width={1920} height={857} alt="streamVibe banner" className="w-full h-full object-cover" />
+        <div className="relative w-full h-screen">
+            <Image src="/images/home-page-banner.png" width={1920} height={857} alt="streamVibe banner" className="object-cover w-full h-full" />
             <div className="w-full h-[10vh] bg-gradient-to-t from-c-black to-c-black/0 absolute bottom-0"></div>
         </div>
     );
